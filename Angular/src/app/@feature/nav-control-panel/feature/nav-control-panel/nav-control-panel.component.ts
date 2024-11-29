ede2ed0e92f7d90ed76647a51af09406b93eadf5
Zagyva Ákos Bence <fortunevideok@gmail.com>
2024-11-29 09:58:11 +0100

    faszom kivan már

--- Angular/src/app/@feature/nav-control-panel/feature/nav-control-panel/nav-control-panel.component.ts
+++ Angular/src/app/@feature/nav-control-panel/feature/nav-control-panel/nav-control-panel.component.ts
@@ -6,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
@@ -17,6 +18,7 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
     // RouterOutlet,
     // RouterLink,
     // RouterLinkActive,
+    MatSidenavModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
